f1115902a0a503be591216bd3d4c97125737ccba
yjoonjang <yjoonjang34@gmail.com>
2023-11-20 15:05:06 +0900

    fix: fix webdriver errors & get all pages

--- jobkorea.py
+++ jobkorea.py
@@ -1,9 +1,34 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+import time
+import urllib.request
+import ssl
+import os
+import requests
+
+# ssl certification 오류 해결
+ssl._create_default_https_context = ssl._create_unverified_context
+
+
+# selenium option
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument(
+    "user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.93 Safari/537.36"
+)
+
+driver = webdriver.Chrome(
+    service=Service(ChromeDriverManager().install()),
+    options=chrome_options.add_argument("--headless"),
+)
 
 array = []
-driver = webdriver.Chrome("/opt/homebrew/bin/chromedriver")
-for i in range(1, 2):
+
+for i in range(1, 50):
     driver.get("https://www.jobkorea.co.kr/starter/PassAssay?schPart=10031&schWork=&schEduLevel=&schCType=&schGroup=&isSaved=1&isFilterChecked=1&OrderBy=0&schTxt=&page=" + str(i))
     # page에 따른 경로
     paper_list = driver.find_element(By.XPATH, "/html/body/div[4]/div[2]/div[2]/div[5]/ul")
